029197bf1929ec0271f3674f4b1bbd334685cbe4
amjadkhansarwar <muhammad.amjad@iths.se>
2022-04-06 11:42:32 +0200

    - Geting data from twitter Api
    - Now appliction has access offtwitter live data
    and applicton is rendrign this live data to client

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# coding-challenge
+# Shortcutter's Coding Challenge
+
+Langugae  Nodejs and using express in Nodejs
+
+Pluggins needs to install
+express, twitter, sockiet.io 
+
+
+Applictioan is geting live data from twitter api and rendring 
+live data to clint.
--- app.js
+++ app.js
@@ -1,19 +1,61 @@
+//-------- configration ---------//
+
 const http = require('http')
 const path = require('path')
 var Twitter = require('twitter');
 const express = require('express')
 const socketIo = require('socket.io')
-const needle = require('needle')
 const config = require('dotenv').config()
-const TOKEN = process.env.TWITTER_BEARER_TOKEN
-const PORT = process.env.PORT || 3000
 
+// --------- assigning values to variables --------//
+
+const PORT = process.env.PORT || 3000
 const app = express()
 
 
+//----- initialize objects of classes ----------//
 const server = http.createServer(app)
+
 const io = socketIo(server)
 
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+  //----------- End point for User -----------//
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './', 'client', 'index.html'))
+    })
+   
+    // --------------- Geting live Streams -------------//
+    function streamTweets(socket) {
+        var stream = client.stream('statuses/filter', {track: 'javascript'});
+        stream.on('data', function(tweet) {
+          try {
+              socket.emit('tweet', tweet)
+            } catch (error) {}
+        });
+         return stream
+    }
 
+     //------------- Sending live data --------------//
+        io.on('connection',  () => {
+          console.log('Client connected...')
+
+          const filteredStream = streamTweets(io)
+
+          let timeout = 0
+          filteredStream.on('timeout', () => {
+            // Reconnect on error
+            console.warn('A connection error occurred. Reconnecting…')
+            setTimeout(() => {
+              timeout++
+              streamTweets(io)
+          }, 2 ** timeout)
+          streamTweets(io)
+          })
+        })
   
   server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
